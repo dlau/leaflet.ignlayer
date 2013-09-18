710e1277244040ac50ce8bf74a44c67b30d77ac6
Daryl L <dlau00@gmail.com>
2013-09-17 22:46:53 -0700

    Update TileLayer.Ign.js

--- TileLayer.Ign.js
+++ TileLayer.Ign.js
@@ -8,14 +8,16 @@ L.TileLayer.Ign = L.TileLayer.extend({
         var urlFormat =
             'http://gpp3-wxs.ign.fr/{key}/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER={layer_name}&STYLE={layer_style}&TILEMATRIXSET=PM&TILEMATRIX={zoom}&TILEROW={y}&TILECOL={x}&FORMAT=image%2F{image_format}';
         if(options.type == 'Parcel'){
-            this._url = urlFormat.replace('{key}', key)
+            this._url = urlFormat
+                            .replace('{key}', key)
                             .replace('{layer_name}', 'CADASTRALPARCELS.PARCELS')
                             .replace('{layer_style}', 'bdparcellaire')
                             .replace('{image_format}', 'png');
         }
         //Default case: aerial imagery
         else{
-            this._url = urlFormat.replace('{key}', key)
+            this._url = urlFormat
+                            .replace('{key}', key)
                             .replace('{layer_name}', 'ORTHOIMAGERY.ORTHOPHOTOS')
                             .replace('{layer_style}', 'normal')
                             .replace('{image_format}', 'jpeg');
@@ -24,7 +26,7 @@ L.TileLayer.Ign = L.TileLayer.extend({
     getTileUrl: function(tilePoint) {
         var zoom = this._getZoomForUrl();
         var numTiles = Math.pow(2, zoom);
-        return this._url.replace('{key}', this._key)
+        return this._url
                 .replace('{zoom}', zoom)
                 .replace('{x}',  (((tilePoint.x % numTiles) + numTiles) % numTiles).toString(10))
                 .replace('{y}', tilePoint.y.toString(10));
